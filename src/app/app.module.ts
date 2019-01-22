cd6c0c9bc5404444d27081b10de1f7dcd68235a7
Tobias Domhardt <tobiasdomhardt@googlemail.com>
2019-01-22 22:03:53 +0100

    add material-modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { MaterialModules } from './material-modules';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { EditComponent } from './components/edit/edit.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-modules.ts
@@ -0,0 +1,10 @@
+import { MatToolbarModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    exports: [
+        MatToolbarModule
+    ]
+})
+
+export class MaterialModules {}
